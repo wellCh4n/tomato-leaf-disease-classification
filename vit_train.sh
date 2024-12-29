341b0464b0ad05c0fb5091be25ce7a128c506d2f
dapao.zhang <917726664@qq.com>
2024-12-29 20:17:59 +0800

    fix train script name

--- vit_train.sh
+++ vit_train.sh
@@ -1,14 +1,14 @@
+# google/vit-base-patch16-224-in21k
 python /home/featurize/transformers/examples/pytorch/image-classification/run_image_classification.py \
-    --model_name_or_path microsoft/resnet-50 \
     --dataset_name wellCh4n/tomato-leaf-disease-image \
-    --output_dir ./tomato_outputs_res_50/ \
+    --output_dir ./tomato_outputs/ \
     --remove_unused_columns False \
     --label_column_name label \
     --do_train \
     --do_eval \
     --learning_rate 2e-5 \
-    --num_train_epochs 100 \
-    --per_device_train_batch_size 16 \
+    --num_train_epochs 5 \
+    --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
     --logging_strategy steps \
     --logging_steps 10 \
@@ -17,5 +17,4 @@ python /home/featurize/transformers/examples/pytorch/image-classification/run_im
     --load_best_model_at_end True \
     --save_total_limit 3 \
     --seed 1337 \
-    --ignore_mismatched_sizes True \
     --report_to tensorboard
